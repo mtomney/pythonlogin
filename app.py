562a4a2d90efb89e4c78cd6cafa33796efa9a5c9
pracproj15 <pracproj15@gmail.com>
2016-07-07 17:38:32 -0400

    Python app was cleaned up a bit, the update handles GET/POST requests at a low level (no security yet)

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
-	return "Hello, world!"
-
-@app.route('/welcome')
 def welcome():
 	return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'user' or request.form['password'] != 'secret':
+			error = 'Invalid username or password, please try again.'
+		else:
+			return redirect(url_for('welcome'))
+	return render_template('login.html', error=error)
 
 if __name__ == '__main__':
 	app.run(debug=True)
