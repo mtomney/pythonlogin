9b7a677efb441a08bf8fb02dbad1a8a1b097d79f
pracproj15 <pracproj15@gmail.com>
2016-07-07 21:05:33 -0400

    Added a basic database and added database connections through the Python app.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # Imported tools and frameworks
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
 app = Flask(__name__)
 
 #Secret key generation and wraps function to force proper authentication prior to accessing welcome page
 app.secret_key = "dont hack me"
+app.database = "sample.db"
 
 def login_required(f):
 	@wraps(f)
@@ -20,7 +22,11 @@ def login_required(f):
 @app.route('/')
 @login_required
 def welcome():
-	return render_template("welcome.html")
+	g.db = connect()
+	query = g.db.execute('select *from posts')
+	posts = [dict(title=row[0], description=row[1]) for row in query.fetchall()]
+	g.db.close()
+	return render_template("welcome.html", posts=posts)
 
 #Handling login and logout functions to allow user access to the main welcome page
 @app.route('/login', methods=['GET', 'POST'])
@@ -41,6 +47,10 @@ def logout():
 	session.pop('logged_in', None)
 	flash('You have been logged out')
 	return redirect(url_for('login'))
+
+#Connect to database
+def connect():
+	return sqlite3.connect(app.database)
 
 #Debug mode on while in creation for ease of testing and edits
 if __name__ == '__main__':
